b0bd1ea031c085b02bdb4422faf53ee7349e1b3d
Tyler Weeres <tweeres04@gmail.com>
2024-09-01 19:41:40 -0700

    Update for 2024/2025

--- lib/getSchedules.ts
+++ lib/getSchedules.ts
@@ -47,27 +47,22 @@ const urls = {
 	kat: {
 		url: 'https://liwsa.com/webapps/spappz_live/schedule_maint',
 		postData:
-			'reg_year=2024&flt_area=cfc&season=All&division=o30&agegroup=All&team_refno=24&stype=All&sname=All&sstat=All&fieldref=All&fdate=9%2F8%2F2023&tdate=2%2F11%2F2024&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
+			'reg_year=2025&flt_area=cfc&season=All&division=o30&agegroup=All&team_refno=24&stype=All&sname=All&sstat=All&fieldref=All&fdate=All&tdate=All&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
 	},
 	mo: {
 		url: 'https://liwsa.com/webapps/spappz_live/schedule_maint',
 		postData:
-			'reg_year=2024&flt_area=sffc&season=All&division=premier&agegroup=All&team_refno=38&stype=All&sname=All&sstat=All&fieldref=All&fdate=9%2F9%2F2023&tdate=2%2F11%2F2024&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
+			'reg_year=2025&flt_area=sffc&season=All&division=premier&agegroup=All&team_refno=38&stype=All&sname=All&sstat=All&fieldref=All&fdate=All&tdate=All&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
 	},
 	tash: {
 		url: 'https://liwsa.com/webapps/spappz_live/schedule_maint',
 		postData:
-			'reg_year=2024&flt_area=sffc&season=All&division=premier&agegroup=All&team_refno=57&stype=All&sname=All&sstat=All&fieldref=All&fdate=9%2F8%2F2023&tdate=2%2F11%2F2024&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
+			'reg_year=2025&flt_area=sffc&season=All&division=premier&agegroup=All&team_refno=57&stype=All&sname=All&sstat=All&fieldref=All&fdate=All&tdate=All&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=liwsa&returnto=&firsttime=0',
 	},
 	nad: {
 		url: 'https://visl.org/webapps/spappz_live/schedule_maint',
 		postData:
-			'reg_year=2024&flt_area=cas&season=All&division=3&sched_pool=All&team_refno=All&stype=All&sname=All&sstat=All&fieldref=All&fdate=9%2F8%2F2023&tdate=3%2F2%2F2024&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=visl&returnto=&firsttime=0',
-	},
-	chris: {
-		url: 'https://visl.org/webapps/spappz_live/schedule_maint',
-		postData:
-			'reg_year=2024&flt_area=cas&season=All&division=5&sched_pool=All&team_refno=159&stype=All&sname=All&sstat=All&fieldref=All&fdate=9%2F8%2F2023&tdate=3%2F2%2F2024&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=visl&returnto=&firsttime=0',
+			'reg_year=2025&flt_area=cas&season=All&division=2&sched_pool=All&team_refno=All&stype=All&sname=All&sstat=All&fieldref=All&fdate=All&tdate=&dow=All&start_time=All&sortby1=sched_time&sortby2=sched_type&sortby3=sched_name&sortby4=None&cmd=Excel&appid=visl&returnto=&firsttime=0',
 	},
 }
 
@@ -96,12 +91,12 @@ async function writeFetchDate() {
 	return fetchDate
 }
 
-type Person = 'nad' | 'mo' | 'kat' | 'tash' | 'chris'
+type Person = 'nad' | 'mo' | 'kat' | 'tash'
 
 async function csvStringsToGames(
 	personToCsvStringFn: (person: Person) => Promise<string | null>
 ) {
-	const people: Person[] = ['nad', 'mo', 'kat', 'tash', 'chris']
+	const people: Person[] = ['nad', 'mo', 'kat', 'tash']
 	const peopleWithCsvString = await Promise.all(
 		people.map(async (person) => ({
 			who: person,
@@ -129,12 +124,11 @@ async function csvStringsToGames(
 }
 
 export async function downloadSchedules() {
-	const [mo, nad, kat, tash, chris, fetchDate] = await Promise.all([
+	const [mo, nad, kat, tash, fetchDate] = await Promise.all([
 		downloadSchedule('mo'),
 		downloadSchedule('nad'),
 		downloadSchedule('kat'),
 		downloadSchedule('tash'),
-		downloadSchedule('chris'),
 		writeFetchDate(),
 	])
 
@@ -143,7 +137,6 @@ export async function downloadSchedules() {
 		nad,
 		kat,
 		tash,
-		chris,
 	}
 
 	const games = await csvStringsToGames((person) =>
